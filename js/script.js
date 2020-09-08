887ceb1c349813a15e4c148f45a9f71338d21934
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-08 15:45:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,16 +27,6 @@ This function will create and insert/append the elements needed to display a "pa
 
 
 
-
-
-
-
-
-
-
-
-
-
 function showPage(list, page) {
 // create two variables which will represent the index for the first and last student on the page
 
@@ -81,38 +71,7 @@ function showPage(list, page) {
    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 showPage(data, 1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -126,6 +85,31 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+   // create a variable to calculate the number of pages needed
+   const numOfPages = Math.ceil(list.length / 9);
+   // select the element with a class of `link-list` and assign it to a variable
+   const linkList = document.querySelector("ul.link-list");
+   // set the innerHTML property of the variable you just created to an empty string
+   linkList.innerHTML = "";
+   // loop over the number of pages needed
+     // create the elements needed to display the pagination button
+     // insert the above elements
+   for (let i = 1; i <= numOfPages; i += 1) {
+      const button = 
+      `<li>
+         <button type ="button">${i}</button>
+      <li>`;
+      linkList.innerHTML += button
+   }
+   // give the first pagination button a class of "active"
+ 
+   // create an event listener on the `link-list` element
+     // if the click target is a button:
+       // remove the "active" class from the previous button
+       // add the active class to the clicked button
+       // call the showPage function passing the `list` parameter and page to display as arguments
+ }
 
-
-// Call functions
+ // Call functions
+ addPagination(data);
