e5e8e8f533707f8138d9bdd780165d6546d6e9ce
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-08 14:24:21 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,107 @@ This function will create and insert/append the elements needed to display a "pa
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showPage(list, page) {
+// create two variables which will represent the index for the first and last student on the page
+
+
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+
+
+// select the element with a class of 'student-list' and assign it to a variable
+  
+   const studentList = document.querySelector("ul.student-list")
+   
+
+// set the innerHTML property of the variable you just created to an empty string
+   
+   studentList.innerHTML = "";
+
+
+// loop over the length of the 'list' parameter
+   // inside the loop create a conditional to display the proper students
+      // inside the conditional:
+      // create the elements needed to display the student information
+      // insert the above elements 
+
+
+   for (let i = 0; i < list.length; i +=1) {
+      if (i >= startIndex && i < endIndex) {
+         const student = list[i];
+         const studentItem = ` 
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src=${student.picture.large} alt="Profile Picture">
+                  <h3>${student.name.first} ${student.name.last}</h3>
+                  <span class="email">${student.email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">${student.registered.date}</span>
+               </div>
+            </li>`;
+         studentList.innerHTML += studentItem;
+      }
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+showPage(data, 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
